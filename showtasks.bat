521303a8de3fabbce359ba48548d0680c6f8231b
ankleszczewski <an.kleszczewski@gmail.com>
2017-11-08 21:23:35 +0100

    17.4 shell exercise

---
+++ showtasks.bat
@@ -0,0 +1,16 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto openURL
+echo.
+echo runcrud has errors - breaking work
+goto fail
+
+:openURL
+start http://localhost:8080/crud/v1/task/getTasks
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
