7668f7370f369b608884729ee041546045087715
ankleszczewski <an.kleszczewski@gmail.com>
2017-11-15 19:46:42 +0100

    18.3 exercise commit

--- src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
+++ src/main/java/com/crud/tasks/domain/CreatedTrelloCard.java
@@ -1,23 +1,26 @@
 package com.crud.tasks.domain;
 
+import com.crud.tasks.domain.badges.Badges;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
-
 @AllArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
-
-    @JsonProperty("name")
-    private String name;
+public class CreatedTrelloCard {
 
     @JsonProperty("id")
     private String id;
 
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("badges")
+    private Badges badges;
+
+    @JsonProperty("shortUrl")
+    private String shorturl;
+
 }
--- src/main/java/com/crud/tasks/domain/badges/Badges.java
+++ src/main/java/com/crud/tasks/domain/badges/Badges.java
@@ -1,23 +1,18 @@
-package com.crud.tasks.domain;
+package com.crud.tasks.domain.badges;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.List;
-
 @AllArgsConstructor
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class Badges {
+    @JsonProperty("votes")
+    private int votes;
 
-    @JsonProperty("name")
-    private String name;
+    @JsonProperty("attachments")
+    private AttachmentsByType attachments;
 
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
 }
