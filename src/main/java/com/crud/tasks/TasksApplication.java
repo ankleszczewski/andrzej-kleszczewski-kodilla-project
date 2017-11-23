aa10782562f8aa9bb7045101e86db7d191a63d5c
ankleszczewski <an.kleszczewski@gmail.com>
2017-11-23 23:09:29 +0100

    19.2 commit

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 //public class TasksApplication extends SpringBootServletInitializer {
 	public class TasksApplication {
 
-	public static void main(String[] args)	{
+	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
