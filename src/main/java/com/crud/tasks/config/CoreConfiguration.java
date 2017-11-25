634562cbdf0be0498f88b46b5ff1ef2f702ebacc
ankleszczewski <an.kleszczewski@gmail.com>
2017-11-25 15:10:11 +0100

    heroku

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -4,33 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-//@EnableSwagger2
 @Configuration
 public class CoreConfiguration {
-        //implements WebMvcConfigurer {
 
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
-
-    //@Bean
-    //public Docket api() {
-        //return new Docket(DocumentationType.SWAGGER_2)
-                //.select()
-                //.apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
-                //.paths(PathSelectors.any())
-                //.build();
-    //}
-
-    //@Override
-    //public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-        // Required by Swagger UI configuration
-        //registry.addResourceHandler("/lib/**").addResourceLocations("/lib/").setCachePeriod(0);
-        //registry.addResourceHandler("/images/**").addResourceLocations("/images/").setCachePeriod(0);
-        //registry.addResourceHandler("/css/**").addResourceLocations("/css/").setCachePeriod(0);
-        //registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-        //registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    //}
 
 }
